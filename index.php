80eb7c3c86d72dd52ff4a72b5dac1f6d47e9afcf
yasinta-web <55954401+yasinta-web@users.noreply.github.com>
2019-09-29 23:10:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Yasinta yang cantik, baik, dan selalu ceriya. mrlc sudah jadi ya. Coba di cek. 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ES GE</title>
+</head>
+
+<body>
+    <p>Yasinta yang cantik, baik, dan selalu ceriya. mrlc sudah jadi ya. Coba di cek. </p>
+</body>
+
+</html>
